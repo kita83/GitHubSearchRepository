6aae4754de20324bfddcdbe8994d54c9bccf2ca3
kita83 <kita83@gmail.com>
2019-09-18 22:27:07 +0900

    Initial Commit

---
+++ GitHubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubSearchRepository
+//
+//  Created by Hiroaki Kitada on 2019/09/18.
+//  Copyright © 2019 Hiroaki Kitada. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
